91f785f23165f5ba08e99c3ec515063e7994b0fb
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-03 12:03:11 -0300

    Vetor - Java

---
+++ Java/vetor.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int n = 10; // Tamanho do Vetor
+        int v[] = new int[n]; // Declaração do vetor "v"
+        int i; // índice ou posição
+        
+    // Entrada de Daddos
+    for (i = 0; i < n; i++) {
+        System.out.printf("Informe %2do. valor de %d: ", (i + 1), n);
+        v[i] = ler.nextInt();
+    }
+    
+    // Processamento: somar todos os valores, definir o maior e o menor valor
+    int soma = 0;
+    int menor = v[0]; // v[0] = 1º valor armazenado no vetor "v"
+    int maior = v[0];
+    for (i =0; i < n; i++) {
+        soma = soma + v[i];
+        
+        if (v[i] < menor) 
+            menor = v[i];
+        
+        if (v[i] > maior) 
+            maior = v[i];
+    }
+    
+    // Saída (Resultados)
+    System.out.printf("\n");
+    for (i = 0; i < n; i++) {
+        if (v[i] == menor)
+            System.out.printf("v[%d] = %2d <------ MENOR VALOR\n", i, v[i]);
+        else if (v[i] == maior)
+            System.out.printf("v[%d] = %2d <------ MAIOR VALOR\n", i, v[i]);
+        else
+            System.out.printf("v[%d] = %2d\n", i, v[i]);
+    }
+    System.out.printf("\nSoma = %d\n", soma);
+    }
+}
